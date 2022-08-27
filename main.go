8deb0e89e27ae2748ae643c07d3974217c65024b
Lennart Espe <3391295+lnsp@users.noreply.github.com>
2022-08-27 17:30:38 +0200

    Use custom struct for talk data in API endpoint

--- main.go
+++ main.go
@@ -877,19 +877,39 @@ func (router *Router) apiWrapper(handler http.Handler) http.Handler {
 }
 
 func (router *Router) apiTalks() http.Handler {
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			talks, err := router.store.UpcomingTalks()
-			if err != nil {
-				logrus.WithError(err).Error("Failed to get upcoming talks")
-				http.Error(w, "failed to get data", http.StatusInternalServerError)
-				return
+	type apiTalk struct {
+		User     string    `json:"user"`
+		Title    string    `json:"title"`
+		Category string    `json:"category"`
+		Date     time.Time `json:"date"`
+		Link     string    `json:"link,omitempty"`
+		Body     string    `json:"body,omitempty"`
+	}
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		talks, err := router.store.UpcomingTalks()
+		if err != nil {
+			logrus.WithError(err).Error("Failed to get upcoming talks")
+			http.Error(w, "failed to get data", http.StatusInternalServerError)
+			return
+		}
+		// Reconstruct as API talks
+		apiTalks := make([]*apiTalk, len(talks))
+		for i, t := range talks {
+			apiTalks[i] = &apiTalk{
+				User:     t.User,
+				Title:    t.Title,
+				Category: t.Category,
+				Date:     t.Date,
+				Link:     t.Link,
+				Body:     t.Body,
 			}
-			if err := json.NewEncoder(w).Encode(talks); err != nil {
-				logrus.WithError(err).Error("Failed to encode response")
-				http.Error(w, "failed to encode response", http.StatusInternalServerError)
-				return
-			}
-		})
+		}
+		if err := json.NewEncoder(w).Encode(apiTalks); err != nil {
+			logrus.WithError(err).Error("Failed to encode response")
+			http.Error(w, "failed to encode response", http.StatusInternalServerError)
+			return
+		}
+	})
 }
 
 func (router *Router) category() http.Handler {
