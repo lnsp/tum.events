42d0430d7ea2f42512fd6e13dfd4afbd2942836e
Lennart Espe <3391295+lnsp@users.noreply.github.com>
2024-06-09 13:16:04 +0200

    Add tumuchdata.club to cron data sources.

--- cron/cron.go
+++ cron/cron.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 	"strings"
 	"time"
@@ -10,13 +12,25 @@ import (
 	"github.com/lnsp/tum.events/blob"
 	"github.com/lnsp/tum.events/kv"
 	"github.com/lnsp/tum.events/structs"
+	"github.com/microcosm-cc/bluemonday"
 	"github.com/mmcdole/gofeed"
+	"gopkg.in/yaml.v2"
+)
+
+var (
+	cronUser = flag.String("user", "ga87fey", "Cron user to submit talks")
+	dryRun   = flag.Bool("dryrun", true, "Print talks to-be-inserted instead of submitting them")
 )
 
 func main() {
+	flag.Parse()
 	if err := run(); err != nil {
 		log.Fatal(err)
 	}
+}
+
+type TalkSource interface {
+	Fetch() ([]*structs.Talk, error)
 }
 
 func run() error {
@@ -35,15 +49,52 @@ func run() error {
 	for _, t := range talks {
 		existingTalks[t.Link] = struct{}{}
 	}
-	fmt.Println("Existing:", existingTalks)
 
+	srcs := []TalkSource{TUMuchData{}, NetInTum{}}
+	scraped := []*structs.Talk{}
+	for _, src := range srcs {
+		fetched, err := src.Fetch()
+		if err != nil {
+			log.Printf("Fetch talks from %T: %s", src, err)
+			continue
+		}
+		scraped = append(scraped, fetched...)
+	}
+
+	count := 0
+	for _, talk := range scraped {
+		// to be considered the same, both title and url must be the same
+		if _, ok := existingTalks[talk.Link]; ok {
+			continue
+		}
+		talk.User = *cronUser
+		if !*dryRun {
+			// now we can feed it
+			if err := storage.InsertTalk(talk); err != nil {
+				return err
+			}
+		}
+		fmt.Println("Inserted talk", talk.Title)
+		count++
+	}
+
+	if !*dryRun {
+		log.Printf("Inserted %d talks", count)
+	}
+
+	return nil
+}
+
+type NetInTum struct{}
+
+func (NetInTum) Fetch() ([]*structs.Talk, error) {
 	// fetch talk data
 	fp := gofeed.NewParser()
 	feed, err := fp.ParseURL("https://www.net.in.tum.de/rss/talks.xml")
 	if err != nil {
-		return err
+		return nil, err
 	}
-	count := 0
+	talks := []*structs.Talk{}
 	for _, item := range feed.Items {
 		titles := strings.Split(item.Custom["content"], "\n")
 		descriptions := strings.Split(item.Description, "\n")
@@ -58,33 +109,64 @@ func run() error {
 			}
 			published, err := time.Parse("02.01.2006 15:04 -0700", fmt.Sprintf("%s 16:00 +0100", item.Published))
 			if err != nil {
-				return err
+				return nil, err
 			}
 			if time.Now().After(published) {
 				continue
 			}
-			talk := &structs.Talk{
+			talks = append(talks, &structs.Talk{
 				Category: kind,
-				User:     "ga87fey",
 				Title:    descriptions[i],
 				Date:     published,
 				Link:     item.Link,
-			}
-			fmt.Println(talk)
-			// to be considered the same, both title and url must be the same
-			if _, ok := existingTalks[talk.Link]; ok {
-				continue
-			}
-			// now we can feed it
-			if err := storage.InsertTalk(talk); err != nil {
-				return err
-			}
-			fmt.Println("Inserted talk", talk.Title)
-			count++
+			})
 		}
 	}
+	return talks, nil
+}
 
-	log.Printf("Inserted %d talks", count)
+type TUMuchData struct{}
 
-	return nil
+func (TUMuchData) Fetch() ([]*structs.Talk, error) {
+	eventsYamlSrc := "https://raw.githubusercontent.com/tumuchdata/tumuchdata.github.io/main/data/events.yaml"
+	eventsYamlResp, err := http.Get(eventsYamlSrc)
+	if err != nil {
+		return nil, err
+	}
+	defer eventsYamlResp.Body.Close()
+
+	decoder := yaml.NewDecoder(eventsYamlResp.Body)
+	events := []struct {
+		Title        string `yaml:"title"`
+		Description  string `yaml:"description"`
+		StartTimeStr string `yaml:"start"`
+		Location     string `yaml:"location"`
+	}{}
+	if err := decoder.Decode(&events); err != nil {
+		return nil, err
+	}
+
+	berlinTz, _ := time.LoadLocation("Europe/Berlin")
+	talks := []*structs.Talk{}
+	for _, e := range events {
+		timestamp, _ := time.ParseInLocation("2006-01-02T15:04:05", e.StartTimeStr, berlinTz)
+		if time.Now().After(timestamp) {
+			continue
+		}
+
+		sanitizer := bluemonday.StrictPolicy()
+		description := sanitizer.Sanitize(e.Description)
+
+		headline, _, _ := strings.Cut(description, "\n")
+		headline, _ = strings.CutPrefix(headline, "Weekly Paper Reading Group. ")
+
+		talks = append(talks, &structs.Talk{
+			Category: "databases",
+			Title:    sanitizer.Sanitize(e.Title) + " - " + headline,
+			Link:     "https://www.tumuchdata.club/events/#" + e.StartTimeStr,
+			Date:     timestamp,
+		})
+	}
+
+	return talks, nil
 }
--- go.mod
+++ go.mod
@@ -26,8 +26,11 @@ require (
 )
 
 require (
-	github.com/PuerkitoBio/goquery v1.8.1 // indirect
+	github.com/PuerkitoBio/goquery v1.9.2 // indirect
 	github.com/andybalholm/cascadia v1.3.2 // indirect
+	github.com/antchfx/htmlquery v1.2.3 // indirect
+	github.com/antchfx/xmlquery v1.2.4 // indirect
+	github.com/antchfx/xpath v1.1.8 // indirect
 	github.com/aws/aws-sdk-go-v2/aws/protocol/eventstream v1.6.0 // indirect
 	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.15.0 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/configsources v1.3.0 // indirect
@@ -45,16 +48,26 @@ require (
 	github.com/aymerick/douceur v0.2.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/go-chi/chi/v5 v5.0.12 // indirect
+	github.com/gobwas/glob v0.2.3 // indirect
+	github.com/gocolly/colly/v2 v2.1.0 // indirect
+	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
+	github.com/golang/protobuf v1.4.2 // indirect
 	github.com/gorilla/css v1.0.1 // indirect
 	github.com/gorilla/securecookie v1.1.2 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/kennygrant/sanitize v1.2.4 // indirect
 	github.com/mmcdole/goxpp v0.0.0-20181012175147-0068e33feabf // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	golang.org/x/net v0.21.0 // indirect
-	golang.org/x/sys v0.17.0 // indirect
+	github.com/saintfish/chardet v0.0.0-20120816061221-3af4cd4741ca // indirect
+	github.com/temoto/robotstxt v1.1.1 // indirect
+	golang.org/x/net v0.24.0 // indirect
+	golang.org/x/sys v0.19.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
+	google.golang.org/appengine v1.6.6 // indirect
+	google.golang.org/protobuf v1.24.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.0 // indirect
 )
